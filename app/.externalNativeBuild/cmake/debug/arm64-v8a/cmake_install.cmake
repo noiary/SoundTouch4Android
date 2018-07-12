ef8ea458a67d2642fd77157dcbda12d6f358f836
maodq <noiary@126.com>
2018-07-12 10:33:47 +0800

    init

--- app/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/maodq/Project/demo/NDKDemo/app
+# Install script for directory: /Users/maodq/Project/demo/soundtouch/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/maodq/Project/demo/NDKDemo/app/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/maodq/Project/demo/soundtouch/app/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
